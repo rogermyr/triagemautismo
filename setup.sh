74f4879d03f8229d25fccdaff1a2312f22238a11
unknown <roro.silva99999@gmail.com>
2023-03-26 19:05:44 -0300

    segunda versao

--- setup.sh
+++ setup.sh
@@ -1,9 +1,16 @@
 mkdir -p ~/.streamlit/
+
 echo "\
 [general]\n\
 email = \"roro.silva99999@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 echo "\
+[theme]
+primaryColor = ‘#84a3a7’
+backgroundColor = ‘#EFEDE8’
+secondaryBackgroundColor = ‘#fafafa’
+textColor= ‘#424242’
+font = ‘sans serif’
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
