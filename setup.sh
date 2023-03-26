88c1c05aef4c1ef313a163912e7ed54214117960
unknown <roro.silva99999@gmail.com>
2023-03-26 19:58:30 -0300

    primeira versao

--- setup.sh
+++ setup.sh
@@ -4,4 +4,16 @@ echo "\
 [general]\n\
 email = \"roro.silva99999@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
-
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+\n\
+[theme]\n\
+primaryColor = '#eb4034'\n\
+backgroundColor = '#021d24'\n\
+secondaryBackgroundColor = '#B9F1C0'\n\
+textColor = '#FFFFFF'\n\
+font = 'sans serif'\n\
+" > ~/.streamlit/config.toml
