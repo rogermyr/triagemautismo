03a0d441676e497c246265da1ebe579ad0dd0a84
unknown <roro.silva99999@gmail.com>
2023-03-26 19:29:32 -0300

    segunda versao

--- setup.sh
+++ setup.sh
@@ -1,18 +1,9 @@
 mkdir -p ~/.streamlit/
 
 echo "\
-[theme]\n\
-primaryColor = '#FF0000'\n\
-backgroundColor = '#00325B'\n\
-secondaryBackgroundColor = '#55B2FF'\n\
-textColor="#FFFFFF"\n\
-"
-
-echo "\
 [general]\n\
 email = \"roro.silva99999@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
-
 echo "\
 [server]\n\
 headless = true\n\
