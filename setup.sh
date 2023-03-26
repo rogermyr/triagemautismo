53b7c68c580f50e25273136e4e9acd8dc3bb8419
unknown <roro.silva99999@gmail.com>
2023-03-26 19:21:18 -0300

    segunda versao

--- setup.sh
+++ setup.sh
@@ -1,12 +1,11 @@
 mkdir -p ~/.streamlit/
-
-echo "\
-[general]\n\
-email = \"roro.silva99999@gmail.com\"\n\
-" > ~/.streamlit/credentials.toml
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
+echo "[theme]
+primaryColor=’#020202’
+backgroundColor=’#c4c3c3’
+secondaryBackgroundColor=’#ebd316’
+font = ‘sans serif’
+[server]
+headless = true
+port = $PORT
+enableCORS = false
 " > ~/.streamlit/config.toml
